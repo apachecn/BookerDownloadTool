be845fdbe0393cd594dd5d5d9a9a2838d3ea5522
wizardforcel <562826179@qq.com>
2022-08-19 11:36:36 +0800

    2022-08-19 11:36:36

---
+++ BookerDownloadTool/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from .pypi import *
+from .npm import *
+from .docker import *
+from .ebook2site import *
+from .libgen import *
+
+"""BookerDownloadTool
+https://github.com/apachecnBookerDownloadTool"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "SATA"
+__version__ = "2022.8.19.0"
